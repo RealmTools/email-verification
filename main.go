34d2df8d23e480b606a725f417b3e52e88fd3b55
kylescottw <info@kylescottw.com>
2022-05-11 01:33:54 -0600

    feat: email validation

--- main.go
+++ main.go
@@ -1,37 +1,71 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"net"
-	"os"
+	"regexp"
 	"strings"
 
 	"github.com/RealmTools/email-verification/meta"
 )
 
 
-func checkIsDisposable(domain string) bool {
+type EmailInformation struct {
+	Username string `json:"username"`
+	Domain   string `json:"domain"`
+	Valid    bool   `json:"valid"`
+}
 
-	
+// ParseEmail: returns email username, domain, and checks if the format is valid
+func ParseEmail(email string) EmailInformation {
+
+	isAddressValid := IsEmailAddressValid(email)
+	if !isAddressValid {
+		return EmailInformation{Valid: false}
+	}
+
+	idx := strings.LastIndex(email, "@")
+	username := email[:idx]
+	domain := strings.ToLower(email[idx+1:])
+
+	return EmailInformation{
+		Username: username,
+		Domain:   domain,
+		Valid:    isAddressValid,
+	}
+}
+
+
+// IsEmailAddressValid: checks email format using regex
+func IsEmailAddressValid(email string) bool {
+	var emailRegexString = "^(?:(?:(?:(?:[a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(?:\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|(?:(?:\\x22)(?:(?:(?:(?:\\x20|\\x09)*(?:\\x0d\\x0a))?(?:\\x20|\\x09)+)?(?:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(?:(?:(?:\\x20|\\x09)*(?:\\x0d\\x0a))?(\\x20|\\x09)+)?(?:\\x22))))@(?:(?:(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])(?:[a-zA-Z]|\\d|-|\\.|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(?:(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])(?:[a-zA-Z]|\\d|-|\\.|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$"
+	var emailRegex = regexp.MustCompile(emailRegexString)
+	return emailRegex.MatchString(email)
+}
+
+
+
+// checkIsDisposable: references a collection of known throwaway domains
+func checkIsDisposable(domain string) bool {
 	return meta.ThrowawayDomains[domain]
 }
 
 
+
 func main() {
 
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("Type a domain name: IE apple.com\n")
-
-	for scanner.Scan() {
-		checkDomain(scanner.Text())
+	var tempEmail = "contact@realmtools.com"
+	email_information := ParseEmail(tempEmail)
+	
+	if email_information.Valid == false {
+		log.Fatal("Error: email is invalid\n")
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal("Error: could not read from input: %v\n", err)
-	}
+	checkDomain(email_information.Domain)
 }
+
+
 
 func checkDomain(domain string) {
 
